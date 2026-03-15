f8f4f62f8f3435ec21cb588c581cbd4a244d8236
Ankit Gochare <ankitgochare35@gmail.com>
2026-03-15 21:06:03 +0530

    fixed problems

--- notes-project2/src/db/db.js
+++ notes-project2/src/db/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 async function ConnectDB(){
-    await mongoose.connect("mongodb+srv://notes_user01:0OQeDRpv2wTghbz0@backend-ankur.6qvuswk.mongodb.net/notes2");
+    await mongoose.connect("");
     console.log("Connected to DB");
 }
 
